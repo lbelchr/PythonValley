98ea036c481cc96969cb388f7a091664f4ce7cb7
lucasbelcher <lbelcher@crescendocollective.com>
2023-10-07 11:44:54 -0500

    merchant and audio

--- code/level.py
+++ code/level.py
@@ -9,6 +9,7 @@ from transition import Transition
 from soil import SoilLayer
 from sky import Rain, Sky
 from random import randint
+from menu import Menu
 
 
 class Level:
@@ -30,9 +31,20 @@ class Level:
 
         # sky
         self.rain = Rain(self.all_sprites)
-        self.raining = randint(0, 10) > 3
+        self.raining = randint(0, 10) > 7
         self.soil_layer.raining = self.raining
         self.sky = Sky()
+
+        # shop
+        self.menu = Menu(self.player, self.toggle_shop)
+        self.shop_active = False
+
+        # sound
+        self.success = pygame.mixer.Sound('./audio/success.wav')
+        self.success.set_volume(0.3)
+        self.music = pygame.mixer.Sound('./audio/music.mp3')
+        self.music.set_volume(.1)
+        self.music.play(loops=-1)
 
     def setup(self):
         tmx_data = load_pygame('./data/map.tmx')
@@ -81,8 +93,12 @@ class Level:
                     self.collision_sprites,
                     self.tree_sprites,
                     self.interaction_sprites,
-                    self.soil_layer)
+                    self.soil_layer,
+                    self.toggle_shop)
             if obj.name == 'Bed':
+                Interaction((obj.x, obj.y), (obj.width, obj.height), (self.interaction_sprites,), obj.name)
+
+            if obj.name == 'Trader':
                 Interaction((obj.x, obj.y), (obj.width, obj.height), (self.interaction_sprites,), obj.name)
 
         # create the ground
@@ -96,6 +112,11 @@ class Level:
     def player_add(self, item):
 
         self.player.item_inventory[item] += 1
+        self.success.play()
+
+    def toggle_shop(self):
+
+        self.shop_active = not self.shop_active
 
     def reset(self):
         # plants
@@ -110,7 +131,7 @@ class Level:
         # soil
         self.soil_layer.remove_water()
         # randomize the rain
-        self.raining = randint(0, 10) > 3
+        self.raining = randint(0, 10) > 7
         self.soil_layer.raining = self.raining
         if self.raining:
             self.soil_layer.water_all()
@@ -128,18 +149,24 @@ class Level:
                     self.soil_layer.grid[plant.rect.centery // TILE_SIZE][plant.rect.centerx // TILE_SIZE].remove('P')
 
     def run(self, dt):
+
+        # drawing logic
         self.display_surface.fill('black')
         self.all_sprites.custom_draw(self.player)
-        self.all_sprites.update(dt)
-        self.plant_collision()
 
+        # updates
+        if self.shop_active:
+            self.menu.update()
+        else:
+            self.all_sprites.update(dt)
+            self.plant_collision()
+
+        # weather
         self.overlay.display()
 
-        # rain
-        if self.raining:
+        if self.raining and not self.shop_active:
             self.rain.update()
 
-        # daytime
         self.sky.display(dt)
 
         # transition overlay
